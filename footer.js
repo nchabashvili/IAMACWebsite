df9472b51000c36f7bfa3b90f8aec6f34171d670
Nodar Chabashvili <dachi1014@gmail.com>
2024-10-20 20:26:07 +0200

    added footer to every page

---
+++ footer.js
@@ -0,0 +1,13 @@
+const footerContent = `
+    <footer>
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <ul>
+            <li><a href="https://www.instagram.com/iamac_architects/">INSTAGRAM</a></li>
+        </ul>
+    </footer>
+`;
+
+// Inject the footer into the page
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.insertAdjacentHTML("beforeend", footerContent);
+});
